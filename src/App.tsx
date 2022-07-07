c432b690a4daa361e7421d985feacc16cc1f63d7
alpetrushkin <alpetrushkin1984@mail.ru>
2022-07-07 06:39:33 +0300

    Commit-1

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to ho to reload.
+           Edit <code>src/App.tsx</code> and save to ho to reload.
         </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt fugiat in inventore ipsam minus odio officia quae tempore. Aut eaque repudiandae saepe soluta ut. Asperiores ducimus eaque expedita facere libero?</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React333
         </a>
       </header>
     </div>
